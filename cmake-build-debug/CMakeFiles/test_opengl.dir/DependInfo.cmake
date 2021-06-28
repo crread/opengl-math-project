0cefdce5a7bbf368dd7ab4d2233c356054a35445
unknown <mickaelberthelot@outlook.fr>
2021-06-28 14:12:42 +0200

    save

--- cmake-build-debug/CMakeFiles/test_opengl.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/test_opengl.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/berthelot Mickael/Desktop/opengl projets/Dragon3DTexture/Dragon3DLight.cpp" "C:/Users/berthelot Mickael/Desktop/opengl projets/Dragon3DTexture/cmake-build-debug/CMakeFiles/Dragon3DTexture.dir/Dragon3DLight.cpp.obj"
+  "C:/Users/berthelot Mickael/Desktop/opengl projets/opengl-math-project/Dragon3DLight.cpp" "C:/Users/berthelot Mickael/Desktop/opengl projets/opengl-math-project/cmake-build-debug/CMakeFiles/test_opengl.dir/Dragon3DLight.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -12,9 +12,6 @@ set(CMAKE_CXX_COMPILER_ID "GNU")
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../GLFW/include"
   "../glew/include"
-  "../Headers/glm"
-  "../Sources"
-  "../Headers"
   )
 
 # Targets to which this target links.
