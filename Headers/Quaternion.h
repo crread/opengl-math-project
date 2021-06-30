02237e2ac2e0b4477a1a4aa48d4e18c6315430d7
quent <quentin2000.fert@gmail.com>
2021-06-30 18:14:22 +0200

    Quaternion.h

--- Headers/Quaternion.h
+++ Headers/Quaternion.h
@@ -9,7 +9,19 @@
 class Quaternion {
 public:
     Quaternion();
-};
+}
+
+ void Quaternion::operator+(const Quaternion *quat);
+ void Quaternion::normalization();
+ Quaternion Quaternion::Conjugation();
+ Quaternion Quaternion::operator* (const Quaternion *rq) const;
+ glm::mat4 glm::mat4::QuaterniontoMatrix();
+ float float::produitsqualaire(const glm::mat4 *mat);
+ Quaternion MatrixtoQuaternion(const glm::mat4 *src);
+ 
+ void setY(float y);
+
+;
 
 
 #endif //OPENGL_PROJECT_QUATERNION_H
