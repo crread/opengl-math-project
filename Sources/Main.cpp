df403e2eb919e3137d97d3fccf3fd9c661655cea
quent <quentin2000.fert@gmail.com>
2021-07-28 15:25:14 +0200

    try to deal with main

--- Sources/Main.cpp
+++ Sources/Main.cpp
@@ -2,10 +2,12 @@
 // Created by berthelot Mickael on 08/05/2021.
 //
 
-#include "Quaternion.cpp"
+
+#include "../Headers/Quaternion.h"
 
 
 int main(int argc, char *argv[])
 {
-    return 1;
+   glm::vec3 vector = std::vector<float> {0, 0, 0};
+    Quaternion q = new Quaternion(vector,0.5);
 }
